a69c111f913cd1b931554e15db6538cba7b2a7ca
kieraeobrien <kieraobrien@Kieras-MacBook-Pro.local>
2020-04-26 14:17:46 -0800

    edited ggplot

--- app.R
+++ app.R
@@ -17,26 +17,6 @@ rsconnect::setAccountInfo(name='kieraobrien',
 # It is on the CCES website. I used it to help select useful questions.
 # https://cces.gov.harvard.edu/, under each year. 
 
-# reading in Stata file of CCES 2018 data
-
-cces2018 <- read_excel("cces2018.xlsx")
-
-View(cces2018)
-
-# creating ggplot and assigning as object
-
-figure1 <- ggplot(cces2018, 
-                  aes(x = cces2018$CO2)) + 
-  geom_bar(mapping = NULL, fill = "mediumseagreen", 
-           color = "mediumseagreen") + 
-  labs(title = "Should the EPA be given the power to regulate CO2 emissions?", 
-       x = "Support                                                      Oppose", 
-       y = "Count", 
-       caption = "Based on data from the Cooperative Congressional Election Study, 2018") + 
-  scale_x_discrete(labels=c("1" = "Support", "2" = "Oppose")) + 
-  theme(plot.title = element_text(face = "bold", 
-                                  size = 17))
-
 # creating shiny app
 
 ui <- fluidPage(
