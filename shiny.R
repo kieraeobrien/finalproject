b9dc504005db9e6cc96785c79b04f82851768663
kieraeobrien <kieraobrien@Kieras-MacBook-Pro.local>
2020-04-26 13:06:57 -0800

    script updates with new edited dataset

--- shiny.R
+++ shiny.R
@@ -7,6 +7,7 @@ library(vctrs)
 library(rsconnect)
 library(shiny)
 library(haven)
+library(readxl)
 
 rsconnect::setAccountInfo(name='kieraobrien',
                           token='A2676BA59250DC21055EE8E018BF62C4',
@@ -16,26 +17,16 @@ rsconnect::setAccountInfo(name='kieraobrien',
 # It is on the CCES website. I used it to help select useful questions.
 # https://cces.gov.harvard.edu/, under each year. 
 
-# reading in Stata file of CCES 2010 data
-
-cces_2010 <- read_dta("/Users/kieraobrien/finalproject/cces_2010.dta")
-
-cces_2010 <- as.data.frame(cces_2010)
-
-View(cces_2010)
-
 # reading in Stata file of CCES 2018 data
 
-cces_2018 <- read_dta("/Users/kieraobrien/finalproject/cces_2018.dta")
+cces2018 <- read_excel("cces2018.xlsx")
 
-cces_2018 <- as.data.frame(cces_2018)
-
-View(cces_2018)
+View(cces2018)
 
 # creating ggplot and assigning as object
 
-figure1 <- ggplot(cces_2018, 
-                  aes(x = cces_2018$CC18_415a)) + 
+figure1 <- ggplot(cces2018, 
+                  aes(x = cces2018$CO2)) + 
   geom_bar(mapping = NULL, fill = "mediumseagreen", 
            color = "mediumseagreen") + 
   labs(title = "Should the EPA be given the power to regulate CO2 emissions?", 
